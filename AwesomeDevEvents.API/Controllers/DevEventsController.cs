2166195666f01db1c8656133acd6216f5cb38ba0
NEPRE\rsilva <renan.silva@nepreconsulting.com>
2024-02-28 09:45:04 -0300

    Realizando a criação de CRUD com ASP.NET core e com Entity Framework

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,161 @@
+﻿using AutoMapper;
+using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Models;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DevEventsController(DevEventsDbContext context, IMapper mapper) { 
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        /// <summary>
+        /// Obter todos os eventos
+        /// </summary>
+        /// <returns> Coleção de eventos</returns>
+        /// <response code="200">Sucesso</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult getAll()
+        {
+            var devEvents = _context.DevEvents.Where(d=> !d.isDelete).ToList();
+
+            var viewModel = _mapper.Map<List<DevEventViewModel>>(devEvents);
+
+            return Ok(viewModel);
+        }
+
+        /// <summary>
+        /// Obter um evento
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <returns>Dados do evento</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não Encontrado</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult getById(Guid id)
+        {
+            var devEvent = _context.DevEvents.Include(de => de.Speakers).SingleOrDefault(d => d.Id == id); //Faz com o que eu trago o primeiro realizando o filtro.
+            if(devEvent == null)
+            {
+                return NotFound();
+            }
+            var viewModel = _mapper.Map<DevEventViewModel>(devEvent);
+            return Ok(viewModel);
+        }
+
+        /// <summary>
+        /// Cadastrar um evento
+        /// </summary>
+        /// <remarks>
+        ///   objeto JSON
+        /// </remarks>
+        /// <param name="id">Dados do evento</param>
+        /// <returns>Objeto recém-criado</returns>
+        /// <response code="201">Sucesso</response>
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult Post([FromBody] DevEventInputModel input)
+        {
+            var devEvent = _mapper.Map<DevEvent>(input);
+            _context.DevEvents.Add(devEvent);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(getById), new { id = devEvent.Id}, devEvent);
+        }
+
+        /// <summary>
+        ///    Atualizar um evento
+        /// </summary>
+        /// <remarks>
+        ///   objeto JSON
+        /// </remarks>
+        /// <param name="id">Identificador do Evento</param>
+        /// <returns>Nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Update(Guid id, DevEventInputModel input)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id); 
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+            _context.DevEvents.Update(devEvent);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        /// <summary>
+        ///    Deletar um evento
+        /// </summary>
+        /// <remarks>
+        ///   objeto JSON
+        /// </remarks>
+        /// <param name="id">Identificador do Evento</param>
+        /// <returns>Nada</returns>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id); 
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            devEvent.Delete();
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+        /// <summary>
+        /// Cadastrar palestrante
+        /// </summary>
+        /// <remarks>
+        ///     objeto JSON
+        /// </remarks>
+        /// <param name="id">Identificador de evento</param>
+        /// <param name="input">Dados do palestrante</param>
+        /// <returns>Nada</returns>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Evento não encontrado</response>
+
+        [HttpPost("{id}/speakers")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult PostSpeaker(Guid id,DevEventSpeakerInputModel input)
+        {
+            var speaker = _mapper.Map<DevEventSpeaker>(input);
+            speaker.DevEventId = id;
+            var devEvent = _context.DevEvents.Any(d => d.Id == id);
+            if (!devEvent)
+            {
+                return NotFound();
+            }
+            _context.DevEventSpeakers.Add(speaker);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
---
+++ AwesomeDevEvents.API/Persistence/Migrations/20240227172855_FirstMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AwesomeDevEvents.API.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EndDate",
+                table: "DevEvents",
+                newName: "End_Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "End_Date",
+                table: "DevEvents",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "End_Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "End_Date",
+                table: "DevEvents",
+                newName: "EndDate");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "DevEvents",
+                type: "End_Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
